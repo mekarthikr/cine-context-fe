fd7e7b9733cec7e3ff279ca97a3d1bc5ed9c1ec2
Karthik R <iamjustkarthik@gmail.com>
2025-06-15 17:53:51 +0530

    update

--- src/components/core/routes/routes.tsx
+++ src/components/core/routes/routes.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
-import { MovieDetailsPage } from '../../../components/movie/movie';
-import { ShowDetailsPage } from '../../../components/show/show';
-import { SearchPages } from '../../../components/search/SearchPage';
-import { ForgotPassword } from '../../../components/auth/forgot-password/ForgotPassword';
+// import { MovieDetailsPage } from '../../../components/movie/movie';
+// import { ShowDetailsPage } from '../../../components/show/show';
+// import { SearchPages } from '../../../components/search/SearchPage';
+// import { ForgotPassword } from '../../../components/auth/forgot-password/ForgotPassword';
+// import { PersonDetailsPage } from '../../../components/profile/Profile';
 import { ThemeProvider } from '../../../components/common/ThemeProvider';
 import { HomePage } from '../../../components/home/Home';
-import { PersonDetailsPage } from '../../../components/profile/Profile';
 
 export const AppRoutes: React.FC = () => (
   <BrowserRouter>
@@ -14,11 +14,11 @@ export const AppRoutes: React.FC = () => (
       <Routes>
         <Route index={true} element={<Navigate to={'/home'} />} />
         <Route path="home" element={<HomePage />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
+        {/* <Route path="forgot-password" element={<ForgotPassword />} />
         <Route path="movie/:movieId" element={<MovieDetailsPage />} />
         <Route path="person/:personId" element={<PersonDetailsPage />} />
         <Route path="show/:showId" element={<ShowDetailsPage />} />
-        <Route path="search" element={<SearchPages />} />
+        <Route path="search" element={<SearchPages />} /> */}
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </ThemeProvider>
