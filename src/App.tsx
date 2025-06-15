d415dcaf7d6746d30b20aaa303351033bef8dfc9
Karthik R <iamjustkarthik@gmail.com>
2025-06-15 18:10:18 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-import { AppRoutes } from './components/core/routes/routes';
+// import { AppRoutes } from './components/core/routes/routes';
+
+import  { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import  { ThemeProvider } from "./components/common";
+import HomePage from "./components/home/Home";
 
 export default function App() {
-  return <AppRoutes />;
+  return   <BrowserRouter>
+  <ThemeProvider defaultTheme="dark" storageKey="cinecontext-theme">
+    <Routes>
+      <Route index={true} element={<Navigate to={'/home'} />} />
+      <Route path="home" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/home" replace />} />
+    </Routes>
+  </ThemeProvider>
+</BrowserRouter>;
 }
