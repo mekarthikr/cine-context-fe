8cac95f120f5437fb640627bf734eb7c30c46461
Karthik R <iamjustkarthik@gmail.com>
2025-06-15 18:03:29 +0530

    update

--- src/components/core/routes/routes.tsx
+++ src/components/core/routes/routes.tsx
@@ -6,14 +6,14 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 // import { ForgotPassword } from '../../../components/auth/forgot-password/ForgotPassword';
 // import { PersonDetailsPage } from '../../../components/profile/Profile';
 import { ThemeProvider } from '../../../components/common/ThemeProvider';
-import { HomePage } from '../../home/Home';
+// import { HomePage } from '../../home/Home';
 
 export const AppRoutes: React.FC = () => (
   <BrowserRouter>
     <ThemeProvider defaultTheme="dark" storageKey="cinecontext-theme">
       <Routes>
         <Route index={true} element={<Navigate to={'/home'} />} />
-        <Route path="home" element={<HomePage />} />
+        {/* <Route path="home" element={<HomePage />} /> */}
         {/* <Route path="forgot-password" element={<ForgotPassword />} />
         <Route path="movie/:movieId" element={<MovieDetailsPage />} />
         <Route path="person/:personId" element={<PersonDetailsPage />} />
